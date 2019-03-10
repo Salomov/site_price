7d132760e7ca2947f8a1976dd6d558fa322c6afd
Pavel Filinkov <info@filinkov.ru>
2019-03-10 23:16:53 +0500

    Collapsed price categories

--- js/site-price.module.js
+++ js/site-price.module.js
@@ -2,14 +2,22 @@
  * @file
  * Misc JQuery scripts in this file
  */
-(function($, window, Drupal, drupalSettings) {
+(function ($, window, Drupal, drupalSettings) {
 
     'use strict';
+
+    $(document).ready(function () {
+        $('.price__group-title').click(function () {
+            $('.price__group-items').hide('slow');
+            var id = $(this).data('group');
+            $('#price__group-items-' + id).show('slow');
+        });
+    });
 
     /**
      * Ajax команда плавной прокрутки страницы к заданному элементу.
      */
-    Drupal.AjaxCommands.prototype.sitePriceScrollTo = function(ajax, response, status) {
+    Drupal.AjaxCommands.prototype.sitePriceScrollTo = function (ajax, response, status) {
         var destination = $(response.object).offset();
         $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, response.duration);
     };
